ff86b1592655214da4bf1d1e5b7b2a506bb338cd
abdullahshaker10 <abdullah.shaker2017@gmail.com>
2022-07-20 01:00:44 +0200

    add admin for store app

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
+from .models import Product, Category
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "type"]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "category"]
--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
+from distutils.command.upload import upload
 from django.db import models
 
 from store.enums import CategoryTypes
+from users.models import CustomUser
 
 
 class Category(models.Model):
@@ -14,3 +16,7 @@ class Product(models.Model):
         to=Category, related_name="products", on_delete=models.CASCADE
     )
     price = models.DecimalField(decimal_places=2, max_digits=2, null=True, blank=True)
+    vendor = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="products"
+    )
+    image = models.ImageField(upload_to="images/")
